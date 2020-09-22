85a05608937b3b38734e485995d992d2e2d406bd
YiMing <onegodan@qq.com>
2020-09-22 11:13:31 +0800

    TankFrame继承Frame，并且重写paint方法。

---
+++ src/com/yiming/tank/TankFrame.java
@@ -0,0 +1,35 @@
+package com.yiming.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @Program: tank
+ * @Description:
+ * @Author: YiMing
+ * @Created: 2020/09/22 10:55
+ */
+public class TankFrame extends Frame {
+
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(200, 200, 50, 50);
+    }
+
+
+}
