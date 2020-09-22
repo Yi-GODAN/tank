eafe33d62e84620f72cc67a002514b75133c813a
YiMing <onegodan@qq.com>
2020-09-22 14:05:58 +0800

    添加键盘处理，根据方向键的按键状态，判断移动的方向

--- src/com/yiming/tank/TankFrame.java
+++ src/com/yiming/tank/TankFrame.java
@@ -36,20 +36,58 @@ public class TankFrame extends Frame {
     public void paint(Graphics g) {
         System.out.println("paint");
         g.fillRect(x, y, 50, 50);
-        x += 10;
-        y += 10;
+        //x += 10;
+        //y += 10;
     }
 
+    // key listener
     class MyKeyListener extends KeyAdapter {
+
+        boolean bU = false;
+        boolean bD = false;
+        boolean bL = false;
+        boolean bR = false;
 
         @Override
         public void keyPressed(KeyEvent e) {
-            System.out.println("key pressed");
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                default:
+                    break;
+            }
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("key released");
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
